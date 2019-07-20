93b3246d60717913d086457a21d6a482ed725618
Harshit Pareek <hp1014@nyu.edu>
2019-07-20 00:37:19 -0700

    feat(adding ag-grid)

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,16 +1,57 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, Element } from '@stencil/core';
 
 @Component({
   tag: 'my-component',
-  styleUrl: 'my-component.css',
+  styleUrl: 'my-component.scss',
   shadow: true
 })
 export class MyComponent {
+  @Element() el: HTMLElement;
+
+  agGrid: any;
+
+  columnDefs = [
+    {headerName: "Make", field: "make"},
+    {headerName: "Model", field: "model"},
+    {headerName: "Price", field: "price"}
+  ];
+
+  rowData = [
+    {make: "Toyota", model: "Celica", price: 35000},
+    {make: "Ford", model: "Mondeo", price: 32000},
+    {make: "Porsche", model: "Boxter", price: 72000}
+  ];
+
+  gridOptions = {
+    columnDefs: this.columnDefs,
+    rowData: this.rowData,
+    onGridReady: function () {
+      this.gridOptions.api.sizeColumnsToFit();
+    }
+  };
+
   render() {
+    console.log('Inside render function');
     return (
-      <div>
-        <p> First Stencil App!</p>
-      </div>
+      <ag-grid
+        id="my-grid"
+        style={{ height: '150px', width: '600px' }}
+        class="ag-theme-balham"
+      >
+
+      </ag-grid>
     );
   }
+
+  componentDidLoad() {
+    console.log('Inside component did load function');
+    this.agGrid = document.querySelector('#my-grid');
+    console.log(this.agGrid);
+  }
+
+  // @Watch('agGrid')
+  // agGridInitializationHandler() {
+  //   this.agGrid.gridOptions = this.gridOptions;
+  // }
+
 }
